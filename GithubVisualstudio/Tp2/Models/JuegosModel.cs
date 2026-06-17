70fc34ecd986be247e256c2f6e85ab0c51b45411
Francisco Jimenez <Fran.jim008@gmail.com>
2026-06-17 00:31:50 -0300

    Api Base Limpia

---
+++ GithubVisualstudio/Tp2/Models/JuegosModel.cs
@@ -0,0 +1,8 @@
+namespace Tp2.Models;
+public class Juegos
+{
+    public int Id {get;set;}
+    public string? Nombre {get;set;}
+    public string? Descripcion {get;set;}
+    public int Costo {get;set;}
+}
---
+++ GithubVisualstudio/Tp2/Models/UsuarioModel.cs
@@ -0,0 +1,9 @@
+namespace Tp2.Models;
+public class Usuarios
+{
+    public int Id {get;set;}
+    public string? Nombre {get;set;}
+    public string? Mail {get;set;}
+    public int Saldo {get;set;}
+    public List<Juegos>? ListaJuegos {get;set;}
+}
